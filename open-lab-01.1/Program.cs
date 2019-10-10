b7b57e5fdc5b40465bb072a58703e58ddc14eaef
martinspilak <martinecko24@gmail.com>
2019-10-10 09:00:52 +0200

    open-lab-01.1

--- open-lab-01.1/Program.cs
+++ open-lab-01.1/Program.cs
@@ -6,12 +6,17 @@ namespace open_lab_01._1
     {
         static void Main(string[] args)
         {
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            int c = a + b;
-            Console.WriteLine(c);
-            
-            
+            var a = Convert.ToInt32(Console.ReadLine());
+            if (a < 10)
+            {
+                Console.WriteLine("This number is less then 10");
+            }
+            else
+            {
+                Console.WriteLine("This number is greater or equal to 10");
+            }
+
+
         }
     }
 }
